57c78f1a3350e0ac763d7517ee2ee43225d4c0cc
Ralph-Su <wenjin@noribear.cn>
2022-05-08 21:20:50 +0800

    optimize send Ding Message Feature

--- src/main/scala/DingMessageSender.scala
+++ src/main/scala/DingMessageSender.scala
@@ -17,50 +17,73 @@ object DingMessageSender extends App {
     * @Param content: 钉钉消息内容
     * @return: void
   **/
-  def sendMarkdownMessage(title : String, content : String) : Unit = {
+  def sendMarkdownMessage(title : String, content : String) : Boolean = {
      val webhook = "https://oapi.dingtalk.com/robot/send?access_token=***"
-     val client : DingTalkClient= new DefaultDingTalkClient(webhook);
-     val request: OapiRobotSendRequest = new OapiRobotSendRequest();
-//    request.setMsgtype("text");
-//    OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
-//    text.setContent("测试文本消息");
-//    request.setText(text);
-//    OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
-//    at.setAtMobiles(Arrays.asList("132xxxxxxxx"));
-//    // isAtAll类型如果不为Boolean，请升级至最新SDK
-//    at.setIsAtAll(true);
-//    at.setAtUserIds(Arrays.asList("109929","32099"));
-//    request.setAt(at);
+     val client : DingTalkClient= new DefaultDingTalkClient(webhook)
+     val request: OapiRobotSendRequest = new OapiRobotSendRequest()
 
-//    request.setMsgtype("link");
-//    OapiRobotSendRequest.Link link = new OapiRobotSendRequest.Link();
-//    link.setMessageUrl("https://www.dingtalk.com/");
-//    link.setPicUrl("");
-//    link.setTitle("时代的火车向前开");
-//    link.setText("这个即将发布的新版本，创始人xx称它为红树林。而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是红树林");
-//    request.setLink(link);
-
+    // 构造消息
     request.setMsgtype("markdown")
     val markdown : OapiRobotSendRequest.Markdown = new OapiRobotSendRequest.Markdown()
     markdown.setTitle(title)
     markdown.setText(content)
     request.setMarkdown(markdown)
 
-//    val at : OapiRobotSendRequest.At= new OapiRobotSendRequest.At();
-//    at.setAtMobiles(Arrays.asList("17333144775"));
-      // isAtAll类型如果不为Boolean，请升级至最新SDK
-//    at.setIsAtAll(true);
-//    at.setAtUserIds(Arrays.asList("109929","32099"))
-//    request.setAt(at);
+    // 设置@某个人或者全体成员
+    val at : OapiRobotSendRequest.At= new OapiRobotSendRequest.At();
+    at.setAtMobiles(Arrays.asList("173xxxx4775"))
+    // isAtAll类型如果不为Boolean，请升级至最新SDK
+    at.setIsAtAll(false);
+    // at.setAtUserIds(Arrays.asList("109929","32099"))
+    request.setAt(at);
 
+    // 发送消息
     val response : OapiRobotSendResponse  = client.execute(request)
-    if (response.isSuccess) println("钉钉消息推送完成。")
-    println(response.getErrmsg, response.getErrcode, response.isSuccess)
-
+    println(response.getErrcode, response.getErrmsg)
+    response.isSuccess
   }
 
-  val title = "Data Task Completed"
-  val content = "中国区产线数据生产完成!"
-  sendMarkdownMessage(title, content)
+  /**
+    * @Description: 发送Text类型的钉钉消息
+    * @Param content: 消息内容
+    * @return: void
+  **/
+  def sendTextMessage(content : String): Boolean = {
+    val webhook = "https://oapi.dingtalk.com/robot/send?access_token="
+    val client : DingTalkClient= new DefaultDingTalkClient(webhook)
+    val request: OapiRobotSendRequest = new OapiRobotSendRequest()
+
+    // 构造消息
+    request.setMsgtype("text")
+    val text : OapiRobotSendRequest.Text = new OapiRobotSendRequest.Text();
+    text.setContent(content)
+    request.setText(text)
+
+    // 发送消息
+    val response = client.execute(request)
+    println(response.getErrcode, response.getErrmsg)
+    response.isSuccess
+  }
+
+//  sendTextMessage("数据")
+
+  def sendTextMessage() : Unit = {
+    val webhook = "https://oapi.dingtalk.com/robot/send?access_token="
+    val client : DingTalkClient= new DefaultDingTalkClient(webhook)
+    val request: OapiRobotSendRequest = new OapiRobotSendRequest()
+
+    request.setMsgtype("link")
+    val link : OapiRobotSendRequest.Link = new OapiRobotSendRequest.Link();
+    link.setMessageUrl("https://www.dingtalk.com/");
+    link.setPicUrl("")
+    link.setTitle("数据时代的火车向前开")
+    link.setText("这个即将发布的新版本，创始人xx称它为红树林。而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是红树林")
+    request.setLink(link)
+
+    // 发送消息
+    val response = client.execute(request)
+    println(response.getErrcode, response.getErrmsg)
+    response.isSuccess
+  }
 
 }
