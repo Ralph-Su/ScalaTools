617153f0056c112f4362e0ad33faa7c8eea3677a
Ralph-Su <wenjin@noribear.cn>
2022-05-22 18:01:36 +0800

    test flink datastream api

---
+++ src/main/scala/flink/datastream/DataStreamAPIExample.scala
@@ -0,0 +1,29 @@
+package flink.datastream
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
+
+object DataStreamAPIExample extends App{
+
+  // create environments of both APIs
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  val tableEnv = StreamTableEnvironment.create(env)
+
+  // create a DataStream
+  val dataStream = env.fromElements("Alice", "Bob", "John")
+
+  // interpret the insert-only DataStream as a Table
+  val inputTable = tableEnv.fromDataStream(dataStream)
+
+  // register the Table object as a view and query it
+  tableEnv.createTemporaryView("InputTable", inputTable)
+  val resultTable = tableEnv.sqlQuery("SELECT UPPER(f0) FROM InputTable")
+
+  // interpret the insert-only Table as a DataStream again
+  val resultStream = tableEnv.toDataStream(resultTable)
+
+  // add a printing sink and execute in DataStream API
+  resultStream.print()
+  env.execute()
+
+}
