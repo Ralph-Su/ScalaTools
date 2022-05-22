ca027d4ba41ea7b94dd2aae8357314f54698afad
Ralph-Su <wenjin@noribear.cn>
2022-05-22 18:27:08 +0800

    test flink datastream api

--- src/test/scala/Test.scala
+++
@@ -1,8 +0,0 @@
-import JodaTime.{loopDatePeriod, loopUTCHourOfDay}
-
-object Test extends App {
-
-   loopUTCHourOfDay("2022-01-01", "yyyy-MM-dd", +8)
-
-   loopDatePeriod("2022-01-01", "2022-01-10", "yyyy-MM-dd")
-}
